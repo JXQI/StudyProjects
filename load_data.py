4ab912b35b46b934f44dc5b1f99b5b55b5603a6b
JXQI <2390139915@qq.com>
2020-10-28 20:50:04 +0800

    add the data of feature

--- load_data.py
+++ load_data.py
@@ -2,6 +2,7 @@ from scipy.io import loadmat
 import pandas as pd
 import os
 from os.path import  join
+from collections import defaultdict
 
 
 #加载数据集
@@ -16,10 +17,16 @@ if not os.path.isdir(path):
 #脑内主要纤维束的特征
 #提取每一个指标：这里将每个病人的特征单独存储一个文件，也就是说每个总共有700个文件，每个8*100
 feature_keys=['FA','MD','RD','AXD','liner','Curvature', 'torsion','volume']
-d_feature={}
+d_feature=defaultdict(list)
+print(d['train_set'].shape)
 for index in range(700):
+    #每个病人的特征分开保存
     for i in range(len(feature_keys)):
-        d_feature[feature_keys[i]]=d['train_set'][0][0][i][index]
+        #八个特征分开
+        for j in range(20):
+            #20个纤维束
+            print(index,i,j)
+            d_feature[feature_keys[i]].append(d['train_set'][j][0][i][index])
     pd.DataFrame.from_dict(data=d_feature,orient='index').to_csv(join(path,str(index)+'.csv'))
 #对年龄性别等指标归类，保存单个数据的特征，比如年龄性别等等
 d_result={}
