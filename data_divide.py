1b7e9f3ced5d6868dc2b18512ff33b9f11e6016d
JXQI <2390139915@qq.com>
2020-10-29 10:33:08 +0800

    change the code to class

--- data_divide.py
+++ data_divide.py
@@ -6,54 +6,67 @@ import pandas as pd
 from matplotlib import pyplot as plt
 
 #用来划分测试集和验证集
-path='./data'
-dict={}
-with open(join(path,'data_result.csv')) as f:
-    next(f)
-    reader=csv.reader(f)
-    for row in reader:
-        dict[row[0]]=row[1:]
-#源数据集中各自占比
-origin={'NC':0,'MCI':0,'AD':0}
-#统计年龄分布
-origin_age={'NC':[],'MCI':[],'AD':[]}
-origin_sex={'NC':[],'MCI':[],'AD':[]}
-for i,num in enumerate(dict['train_diagnose']):
-    if float(num)==1:
-        origin['NC']+=1
-        origin_age['NC'].append(round(float(dict['age'][i]),2))
-        origin_sex['NC'].append(dict['sex'][i])
-    elif float(num)==2:
-        origin['MCI'] += 1
-        origin_age['MCI'].append(round(float(dict['age'][i])))
-        origin_sex['MCI'].append(dict['sex'][i])
-    elif float(num)==3:
-        origin['AD'] += 1
-        origin_age['AD'].append(round(float(dict['age'][i])))
-        origin_sex['AD'].append(dict['sex'][i])
-print(origin)
-count_age={}
-count_sex={}
-for i in origin_sex.keys():
-    sex=Series(origin_sex[i]).value_counts()
-    count_sex[i]=sex
-for i in origin_age.keys():
-    #age=Series(origin_age[i]).value_counts()
-    age = Series(origin_age[i]).mean()
-    count_age[i]=round(age,2)
-#求每个类别的平均年龄
-print(count_age)
-print(count_sex)
-#绘制直方图
-plt.hist(origin_age['AD'],label='AD',alpha=0.4)
-plt.hist(origin_age['NC'],label='NC',alpha=0.4)
-plt.hist(origin_age['MCI'],label='MCI',alpha=0.4)
-plt.legend()
-plt.show()
-#必须有每个列表长度相同才可以用
-# df_age=DataFrame(origin_age)
-# df_sex=DataFrame(origin_sex)
-# df_age(pd.value_counts())
-# df_sex(pd.value_counts())
+class data_statistics:
+    def __init__(self):
+        self.path='./data'
+        #统计患病人数，正常人数
+        self.dict={}
+        # 源数据集中各自占比
+        self.origin = {'NC': 0, 'MCI': 0, 'AD': 0}
+        # 统计年龄分布
+        self.origin_age = {'NC': [], 'MCI': [], 'AD': []}
+        self.origin_sex = {'NC': [], 'MCI': [], 'AD': []}
+    #加载数据
+    def data_load(self,filename):
+        with open(join(self.path,filename)) as f:
+            next(f)
+            reader=csv.reader(f)
+            for row in reader:
+                self.dict[row[0]]=row[1:]
+    #统计各个类别的数目
+    def data_ratio(self):
+        for i,num in enumerate(self.dict['train_diagnose']):
+            if float(num)==1:
+                self.origin['NC']+=1
+                self.origin_age['NC'].append(round(float(self.dict['age'][i]),2))
+                self.origin_sex['NC'].append(self.dict['sex'][i])
+            elif float(num)==2:
+                self.origin['MCI'] += 1
+                self.origin_age['MCI'].append(round(float(self.dict['age'][i])))
+                self.origin_sex['MCI'].append(self.dict['sex'][i])
+            elif float(num)==3:
+                self.origin['AD'] += 1
+                self.origin_age['AD'].append(round(float(self.dict['age'][i])))
+                self.origin_sex['AD'].append(self.dict['sex'][i])
+
+            #保存每个类别age和sex出现的频次
+            self.count_age={}
+            self.count_sex={}
+            for i in self.origin_sex.keys():
+                #计算频次
+                sex=Series(self.origin_sex[i]).value_counts()
+                self.count_sex[i]=sex
+            for i in self.origin_age.keys():
+                #计算频次
+                #age=Series(origin_age[i]).value_counts()
+                #计算平均值
+                age = Series(self.origin_age[i]).mean()
+                self.count_age[i]=round(age,2)
+    #返回各个类别的数目
+    def get_number_class(self):
+        return self.origin
+    #返回各个类别年龄的均值
+    def get_age(self):
+        return self.count_age
+    #返回各个类别性别的频次
+    def get_sex(self):
+        return self.count_sex
+    def hist_distributed_age_sex(self):
+        # 绘制直方图
+        plt.hist(self.origin_age['AD'], label='AD', alpha=0.4)
+        plt.hist(self.origin_age['NC'], label='NC', alpha=0.4)
+        plt.hist(self.origin_age['MCI'], label='MCI', alpha=0.4)
+        plt.legend()
+        plt.show()
 
 
