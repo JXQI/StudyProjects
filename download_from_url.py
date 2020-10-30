65db89b89b44db58a42a4cde7e60b802b2039411
JXQI <2390139915@qq.com>
2020-10-30 08:58:33 +0800

    add the download_from url

---
+++ download_from_url.py
@@ -0,0 +1,123 @@
+from urllib.request import urlopen,Request
+import os
+import tempfile
+import hashlib
+import shutil
+import sys
+import zipfile
+import warnings
+import torch
+from os.path import join
+
+try:
+    from tqdm.auto import tqdm  # automatically select proper tqdm submodule if available
+except ImportError:
+    try:
+        from tqdm import tqdm
+    except ImportError:
+        # fake tqdm if it's not installed
+        class tqdm(object):  # type: ignore
+
+            def __init__(self, total=None, disable=False,
+                         unit=None, unit_scale=None, unit_divisor=None):
+                self.total = total
+                self.disable = disable
+                self.n = 0
+                # ignore unit, unit_scale, unit_divisor; they're just for real tqdm
+
+            def update(self, n):
+                if self.disable:
+                    return
+
+                self.n += n
+                if self.total is None:
+                    sys.stderr.write("\r{0:.1f} bytes".format(self.n))
+                else:
+                    sys.stderr.write("\r{0:.1f}%".format(100 * self.n / float(self.total)))
+                sys.stderr.flush()
+
+            def __enter__(self):
+                return self
+
+            def __exit__(self, exc_type, exc_val, exc_tb):
+                if self.disable:
+                    return
+
+                sys.stderr.write('\n')
+
+url="https://pan.cstcloud.cn/unode/stor/\
+downloadByUrl?downloadId=1.eyJhZ2UiOjg2NDAwL\
+CJidWNrZXQiOiJkZWZhdWx0IiwiY3RpbWUiOjE2MDM5ODI\
+wMDksImtleSI6IlNLMy13S1BaMXRsT1laLW9BM0tsS3RfRzl\
+JQUFBQUFFSmdFZiIsImxlbiI6Njk1OTk1MTksIm5hbWUiOiJN\
+Q0FEX0FGUV9jb21wZXRpdGlvbi5tYXQiLCJwb3MiOjAsInNpem\
+UiOjY5NTk5NTE5fQ.238564386"
+
+def download_url_to_file(url, dst, hash_prefix=None, progress=True):
+    req=Request(url)
+    u = urlopen(req)
+    meta = u.info()
+    if hasattr(meta, 'getheaders'):
+        content_length = meta.getheaders("Content-Length")
+    else:
+        content_length = meta.get_all("Content-Length")
+    if content_length is not None and len(content_length) > 0:
+        file_size = int(content_length[0])
+    print(file_size/(1024**2))  #单位MB
+    dst = os.path.expanduser(dst)
+    dst_dir = os.path.dirname(dst)
+    f = tempfile.NamedTemporaryFile(delete=False, dir=dst_dir)
+    print(dst,dst_dir,f)
+    try:
+        if hash_prefix is not None:
+            sha256 = hashlib.sha256()
+        with tqdm(total=file_size, disable=not progress,
+                  unit='B', unit_scale=True, unit_divisor=1024) as pbar:
+            while True:
+                buffer = u.read(8192)
+                if len(buffer) == 0:
+                    break
+                f.write(buffer)
+                if hash_prefix is not None:
+                    sha256.update(buffer)
+                pbar.update(len(buffer))
+
+        f.close()
+        if hash_prefix is not None:
+            digest = sha256.hexdigest()
+            if digest[:len(hash_prefix)] != hash_prefix:
+                raise RuntimeError('invalid hash value (expected "{}", got "{}")'
+                                   .format(hash_prefix, digest))
+        shutil.move(f.name, dst)
+    finally:
+        f.close()
+        if os.path.exists(f.name):
+            os.remove(f.name)
+def _legacy_zip_load(filename, model_dir, map_location):
+    warnings.warn('Falling back to the old format < 1.6. This support will be '
+                  'deprecated in favor of default zipfile format introduced in 1.6. '
+                  'Please redo torch.save() to save it in the new zipfile format.')
+    # Note: extractall() defaults to overwrite file if exists. No need to clean up beforehand.
+    #       We deliberately don't handle tarfile here since our legacy serialization format was in tar.
+    #       E.g. resnet18-5c106cde.pth which is widely used.
+    with zipfile.ZipFile(filename) as f:
+        members = f.infolist()
+        if len(members) != 1:
+            raise RuntimeError('Only one file(not dir) is allowed in the zipfile')
+        f.extractall(model_dir)
+        extraced_name = members[0].filename
+        extracted_file = os.path.join(model_dir, extraced_name)
+    return torch.load(extracted_file, map_location=map_location)
+def _is_legacy_zip_format(filename):
+    if zipfile.is_zipfile(filename):
+        infolist = zipfile.ZipFile(filename).infolist()
+        return len(infolist) == 1 and not infolist[0].is_dir()
+    return False
+def download_dataset_from_url(url,path,filename):
+    if filename not  in os.listdir(path):
+        download_url_to_file(url, join(path,filename), hash_prefix=None, progress=True)
+
+if __name__=='__main__':
+    filename='MCAD_AFQ_competition.mat'
+    path='./'
+    download_dataset_from_url(url,path,filename)
---
+++ settings.py
@@ -0,0 +1,14 @@
+#数据集下载链接
+URL="https://pan.cstcloud.cn/unode/stor/\
+downloadByUrl?downloadId=1.eyJhZ2UiOjg2NDAwL\
+CJidWNrZXQiOiJkZWZhdWx0IiwiY3RpbWUiOjE2MDM5ODI\
+wMDksImtleSI6IlNLMy13S1BaMXRsT1laLW9BM0tsS3RfRzl\
+JQUFBQUFFSmdFZiIsImxlbiI6Njk1OTk1MTksIm5hbWUiOiJN\
+Q0FEX0FGUV9jb21wZXRpdGlvbi5tYXQiLCJwb3MiOjAsInNpem\
+UiOjY5NTk5NTE5fQ.238564386"
+
+#保存的文件名和路径
+PATH='./'
+FILENAME='MCAD_AFQ_competition.mat'
+
+
