43120d5f39efad80049603593fba6a74e815761c
JXQI <2390139915@qq.com>
2020-11-11 20:01:03 +0800

    fix the bug

--- loader_mul.py
+++ loader_mul.py
@@ -14,7 +14,7 @@ class dataloader(Dataset):
         self.data_set=data_set+'.txt'
         self.num_class=num_class
         #self.class_d={"NC":1,"MCI":2,'AD':3}        #TODO:这里需要支持三分类
-        self.class_d =['NC','MCI','AD'] if self.num_class==3 else ['NC','AD']
+        self.class_d =['NC','AD','MCI']
         self.features=[]
         self.labels=[]
 
@@ -25,9 +25,13 @@ class dataloader(Dataset):
                 # self.one_hot = np.zeros(3)
                 # self.one_hot[self.class_d.index(line[1])]=1
                 # print(line[1],self.one_hot)
-                print(line[1])
-                self.labels.append(self.class_d.index(line[1]))
-                self.features.append(line[0])
+                if self.num_class==3:
+                    self.labels.append(self.class_d.index(line[1]))
+                    self.features.append(line[0])
+                else:
+                    if line[1]!="MCI":
+                        self.labels.append(self.class_d.index(line[1]))
+                        self.features.append(line[0])
         #这里初始化就将处理好的数据加载进内存中来
         #self.data=deal_all(self.features,self.path,self.transforms)
         self.data=load_data(self_features=self.features,self_path=self.path,self_transforms=self.transforms)
