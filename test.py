48376f47ee8cb0219980062a1928a45540dccfc6
JXQI <2390139915@qq.com>
2020-11-28 15:27:36 +0800

    add the boxes

--- test.py
+++ test.py
@@ -9,6 +9,9 @@ import os
 from PIL import Image
 from matplotlib import pyplot as plt
 import numpy as np
+import os
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
+from random import random
 
 def get_transform(train):
     transforms = []
@@ -30,8 +33,8 @@ dataset_test = PennFudanDataset('PennFudanPed', get_transform(train=False))
 # split the dataset in train and test set
 #indices = torch.randperm(len(dataset)).tolist()
 indices=[i for i in range(len(dataset))]
-dataset = torch.utils.data.Subset(dataset, indices[:-500])
-dataset_test = torch.utils.data.Subset(dataset_test, indices[-500:])
+dataset = torch.utils.data.Subset(dataset, indices[:-50])
+dataset_test = torch.utils.data.Subset(dataset_test, indices[-50:])
 
 # define training and validation data loaders
 data_loader = torch.utils.data.DataLoader(
@@ -44,7 +47,7 @@ data_loader_test = torch.utils.data.DataLoader(
 
 # get the model using our helper function
 model = get_model_instance_segmentation(num_classes)
-model.load_state_dict(torch.load(os.path.join('./Weights',"mask_r_cnn.pt")))
+model.load_state_dict(torch.load(os.path.join('./Weights',"mask_r_cnn.pt"),map_location=torch.device('cpu')))
 # move model to the right device
 model.to(device)
 
@@ -55,34 +58,35 @@ with torch.no_grad():
     prediction=model([img.to(device)])
     image=Image.fromarray(img.mul(255).permute(1,2,0).byte().numpy())
     #显示gd
+    boxes=list(np.array(label['boxes']))        #增加边框显示
     ims_np = np.array(label['masks'], dtype="uint16")
     mask = np.zeros(ims_np[0].shape)
-    print(mask.shape)
     for i in range(len(ims_np)):
         mask += ims_np[i] * (i + 1)  # 为了用不同的颜色显示出来
     mask = Image.fromarray(mask).convert('L')
-    # temp=label['masks'].mul(255).permute(1,2,0).byte().numpy()
-    # if temp.shape[2]>1:
-    #     mask=Image.fromarray(temp)
-    # else:
-    #     mask=Image.fromarray(temp.reshape(temp.shape[:2]))
-    #pre=Image.fromarray(prediction[0]['masks'][3,0].mul(255).byte().cpu().numpy())
+    boxes_pre=prediction[0]['boxes']
     pres=np.zeros(prediction[0]['masks'].shape[1:])
-    print(pres.shape)
     for i in range(prediction[0]['masks'].shape[0]):
-        print(i)
         pres+=prediction[0]['masks'][i, 0].mul((i+1)*255//prediction[0]['masks'].shape[0]).byte().cpu().numpy()
     pre = Image.fromarray(pres[0])
-    #pre = Image.fromarray(prediction[0]['masks'].sum(axis=0)[0].mul(255).cpu().numpy())
-    plt.subplot(1,3,1)
+    fig=plt.figure()
+    ax=fig.add_subplot(1,3,1)
     plt.title('image')
+    for i in boxes:
+        x0,y0,x1,y1=i
+        color=(random(),random(),random())
+        rect=plt.Rectangle((x0,y0),abs(x1-x0),abs(y1-y0),edgecolor=color,fill=False,linewidth=1)
+        ax.add_patch(rect)
     plt.imshow(image)
     plt.subplot(1,3,2)
     plt.title('mask')
     plt.imshow(mask)
-    plt.subplot(1,3,3)
+    ax3=fig.add_subplot(1,3,3)
     plt.title('prection')
-    #pre=pre.convert('L')
+    for i in boxes_pre:
+        x0,y0,x1,y1=i
+        color=(random(),random(),random())
+        rect=plt.Rectangle((x0,y0),abs(x1-x0),abs(y1-y0),edgecolor=color,fill=False,linewidth=1)
+        ax3.add_patch(rect)
     plt.imshow(pre)
-    plt.imshow(pre,cmap='tab10')
     plt.show()
