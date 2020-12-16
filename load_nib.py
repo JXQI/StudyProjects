b2c17e2c693343a7236ea79d577ec0f37045f453
JXQI <2390139915@qq.com>
2020-12-16 18:59:54 +0800

    make the pre.csv

---
+++ load_nib.py
@@ -0,0 +1,47 @@
+"""
+function:通过nib加载文件
+"""
+import nibabel as nib
+import numpy as np
+from skimage.measure import label, regionprops
+import pandas as pd
+import os
+from tqdm import tqdm
+'''
+function: 生成csv文件
+'''
+def _make_submission_files(pred, image_id, affine):
+    pred_label = label(pred > 0).astype(np.int16)
+    pred_regions = regionprops(pred_label, pred)
+    pred_index = [0] + [region.label for region in pred_regions]
+    pred_proba = [0.0] + [region.mean_intensity for region in pred_regions]
+    # placeholder for label class since classifaction isn't included
+    pred_label_code = [0] + [1] * int(pred_label.max())
+    pred_image = nib.Nifti1Image(pred_label, affine)
+    pred_info = pd.DataFrame({
+        "public_id": [image_id] * len(pred_index),
+        "label_id": pred_index,
+        "confidence": pred_proba,
+        "label_code": pred_label_code
+    })
+
+    return pred_image, pred_info
+
+if __name__=='__main__':
+    path="/Users/jinxiaoqiang/jinxiaoqiang/数据集/Bone/nii_test_2/mask"
+    pred_dir="/Users/jinxiaoqiang/jinxiaoqiang/数据集/Bone/nii_test_2/mask"
+
+    image_path_list = sorted([os.path.join(path, file) for file in os.listdir(path) if "nii" in file])
+    image_id_list = [os.path.basename(path).split("-")[0] for path in image_path_list]
+    progress = tqdm(total=len(image_id_list))
+    pred_info_list = []
+    for image_id, image_path in zip(image_id_list, image_path_list):
+        image=nib.load(image_path)
+        image_arr = image.get_fdata().astype(np.uint8)
+        image_affine = image.affine
+
+        pred_image,pred_info=_make_submission_files(image_arr,image_id,image_affine)
+        pred_info_list.append(pred_info)
+        progress.update()
+    pred_info = pd.concat(pred_info_list, ignore_index=True)
+    pred_info.to_csv(os.path.join(pred_dir, "pred_info.csv"),index=False)
